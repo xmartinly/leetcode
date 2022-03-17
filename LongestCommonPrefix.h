e5f79035dc597cfac0c20223c6337ae422ad2545
xmartinly <778567144@qq.com>
2022-03-17 15:14:49 +0800

    2600-LY\eet4  20220317.151449

--- LongestCommonPrefix.h
+++ LongestCommonPrefix.h
@@ -4,9 +4,13 @@
 class LongestCommonPrefix {
 public:
     string longestCommonPrefix(vector<string>& vs_strs) {
-        string s_result = "";
-        for(auto iter = vs_strs.begin(); iter != vs_strs.end(); iter++) {
-            vector<char> vc_str;
+        string s_result = "", s_first = vs_strs.at(0);
+        int i_inputLen = vs_strs.size();
+        int i_firstStrLen = vs_strs.at(0).size();
+        for(int i  = 0; i < i_firstStrLen; i++) {
+//            s_result +=
+            for(int j = 0; j < i_inputLen; j++) {
+            }
         }
         return s_result;
     }
